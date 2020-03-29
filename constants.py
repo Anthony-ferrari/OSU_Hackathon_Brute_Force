9f9d1e94dd91d0ddef6413b38aa12ac75547e851
Daniel Gwon <55669020+danielgwon@users.noreply.github.com>
2020-03-28 23:27:32 -0400

    Initial commit of true cases calculation
    Uses data from 3/28/20

---
+++ constants.py
@@ -0,0 +1,28 @@
+# source: https://medium.com/@tomaspueyo/coronavirus-act-today-or-people-will-die-f4d3d9cd99ca
+
+
+# Proportion of cases that result in death
+FATALITY_RATE = 0.0087          # 0.5% well-prepared, 5% unprepared
+
+"""
+Factors affecting preparedness
+* more people in hospitals means fewer will be treated properly
+- not enough medical equipment
+- not enough PPE for health-care professionals
+- health-care providers must decide who will live/die
+"""
+
+# Number of days it takes for cases to double
+DOUBLING_TIME = 6.18
+
+# Days from infection to death
+DAYS_TO_DEATH = 17.3
+
+
+# number of cases at contraction
+# total deaths / fatality rate
+
+
+# Total deaths as of today
+# get this number from online
+
